44cb05ba3ccf102b89ae77c26f4d933bfec4f503
ala-debbache <65853803+ala-debbache@users.noreply.github.com>
2021-10-02 10:54:50 +0100

    updating index.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export async function getPosts(){
-    let data = await fetch("http://localhost:8080/posts")
+    let data = await fetch("https://powerful-ridge-32851.herokuapp.com/posts")
     .then((data)=>data.json());
     return data;
 }
 
 export async function getPost(id){
-    let data = await fetch(`http://localhost:8080/posts/${id}`)
+    let data = await fetch(`https://powerful-ridge-32851.herokuapp.com/posts/${id}`)
     .then((data)=>data.json());
     return data;
 }
